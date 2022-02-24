13088331fd22d5d799133455f3ad51a564632ac6
Mst. Reba Khatun <shorifulislambd1993@gmail.com>
2022-02-24 23:43:05 +0600

    website

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,15 @@
 <?php 
-	$dbHost = 'localhost';
-	$dbUser = 'root';
-	$dbPass = '';
-	$dbName = 'user_info';
+// local server development part
+	// $dbHost = 'localhost';
+	// $dbUser = 'root';
+	// $dbPass = '';
+	// $dbName = 'user_info';
+
+//Remote database
+	$dbHost = 'remotemysql.com';
+	$dbUser = '4r38G6pQVW';
+	$dbPass = 'XpiRY4OhK4';
+	$dbName = ' 4r38G6pQVW';
 
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 
